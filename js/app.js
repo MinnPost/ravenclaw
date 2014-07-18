3c7915b5170a176f9202795c0f37f2cd8d11bc79
Alan Palazzolo <apalazzolo@minnpost.com>
2014-07-18 15:31:21 -0500

    Ravenclaw :bird:

--- js/app.js
+++ js/app.js
@@ -2,5 +2,42 @@
 
 (function() {
 
-  
+  var translations = {
+    haveyoureadthecodeofconductanddoyouagreetofollowit: 'conduct',
+    othercommentsorquestions: 'comments',
+    titleoftalk: 'title',
+    youremail: 'email',
+    yourname: 'name',
+    yourphonenumber: 'phone'
+  };
+
+  Tabletop.init({
+    key: '18rCcVEVf5Gn2TA65IjncGKDc_TnXOrveuvkFgjTTPz4',
+    callback: renderTalks,
+    simpleSheet: true
+  });
+
+  function renderTalks(talks, tabletop) {
+
+    // Translate
+    talks = _.map(talks, function(t, ti) {
+      _.each(translations, function(n, o) {
+        if (!_.isUndefined(t[o])) {
+          t[n] = t[o];
+        }
+      });
+
+      return t;
+    });
+
+    // Ensure conduct is read
+    talks = _.filter(talks, function(t, ti) {
+      return (t.conduct.toLowerCase() === 'yes');
+    });
+
+    // Render template
+    $('.talk-list').html(_.template($('#talk-list-template').html(), {
+      talks: talks
+    }))
+  }
 })();
